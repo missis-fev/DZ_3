99e2219a5871c1655490bf2a06347168f06ccd82
Elena Frolova <missis-fev@yandex.ru>
2022-10-27 10:16:02 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 23 Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+ 
+Console.Write("Введите число: "); 
+int N = int.Parse(Console.ReadLine()!); 
+int[] GetSqrtTable(int N) 
+{   
+    int[] SqrtArray = new int[N]; 
+    for (int i = 0; i < N; i++) 
+    {  
+        SqrtArray[i] = (i + 1) * (i + 1) * (i + 1);    
+    }      
+    return SqrtArray; 
+}  
+void PrintTable(int[] Array) 
+{
+    int number = 1;     
+    for (int i = 0; i < Array.Length; i++)
+    { 
+     Console.WriteLine(Array[i]); 
+     number++; 
+    }
+}  
+int[] SqrtTable = GetSqrtTable(N);
+PrintTable(SqrtTable);
